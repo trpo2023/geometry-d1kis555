ee5a38b5979bbfaf96839fd0ee3ed2eb4e1390f4
d1kis555 <simskovdrago@gmail.com>
2023-04-23 13:34:52 +0700

    edit geometry.c

--- lab2/Geometry.c
+++ lab2/Geometry.c
@@ -164,3 +164,124 @@ double y_data(char* arr, int* num)
     char* dumpster;
     return strtod(free_space, &dumpster);
 }
+
+double radius_data(char* arr, int* num)
+{
+    char free_space[20];
+    int i = 0;
+    int extra_point_count = 0;
+
+    while (!isdigit(arr[*num])) {
+        if (arr[*num] == ' ' || arr[*num] == ',') {
+            *num += 1;
+        } else {
+            output_bugs(BUG_NUMBER, *num, NULL);
+            exit(1);
+        }
+    }
+
+    while (isdigit(arr[*num]) || arr[*num] == '.') {
+        free_space[i] = arr[*num];
+        i++;
+        *num += 1;
+        if (arr[*num] == '.') {
+            extra_point_count += 1;
+        }
+        if (extra_point_count >= 2) {
+            output_bugs(BUG_EXTRA_POINT, *num, &arr[*num]);
+            exit(1);
+        }
+    }
+
+    while (arr[*num] != ')') {
+        if (arr[*num] == ' ') {
+            *num += 1;
+        } else {
+            if (arr[*num] == '(') {
+                output_bugs(BUG_STAPLES, *num, &arr[*num]);
+                exit(1);
+            } else {
+                output_bugs(BUG_STAPLES_2, *num, &arr[*num]);
+                exit(1);
+            }
+        }
+    }
+    char* dumpster;
+    return strtod(free_space, &dumpster);
+}
+
+void empty(char* arr, int* num)
+{
+    *num += 1;
+    while (arr[*num] != '\n' && arr[*num] != EOF) {
+        if (arr[*num] == ' ') {
+            *num += 1;
+        } else {
+            output_bugs(BUG_UNIDENTIFIED_VARIABLES, *num, NULL);
+            exit(1);
+        }
+    }
+}
+
+struct Point find_center(char* arr, int* num)
+{
+    struct Point Center;
+
+    Center.x = x_data(arr, num);
+    Center.y = y_data(arr, num);
+
+    return Center;
+}
+
+struct Circle find_out_circle(struct Point* Center, char* arr, int* num)
+{
+    struct Circle circle;
+
+    circle.center.x = Center->x;
+    circle.center.y = Center->y;
+    circle.radius = radius_data(arr, num);
+
+    return circle;
+}
+
+void output_circle_message(struct Circle* circle)
+{
+    printf("\ncircle(%.2f %.2f, %.2f)\n",
+           circle->center.x,
+           circle->center.y,
+           circle->radius);
+}
+
+int main()
+{
+    char enter[MAX], shape[MAX];
+    int num = 0;
+
+    printf("Enter the name and coordinates of the shape (or press Enter for "
+           "exit):\n");
+    fgets(enter, MAX, stdin);
+
+    for (int i = 0; i < strlen(enter); i++) {
+        if (enter[i] == '(' || enter[i] == ' ') {
+            to_lower(shape, num);
+            if (strcmp(shape, "circle") == 0) {
+                struct Point center = find_center(enter, &num);
+                struct Circle circle = find_out_circle(&center, enter, &num);
+                empty(enter, &num);
+                output_circle_message(&circle);
+                break;
+            } else {
+                output_bugs(BUG_NAME, 0, NULL);
+                exit(1);
+            }
+        } else if (enter[i] == ')') {
+            output_bugs(BUG_STAPLES, num, &enter[i]);
+            exit(1);
+        }
+
+        shape[num] = enter[i];
+        num++;
+    }
+
+    return 0;
+}
